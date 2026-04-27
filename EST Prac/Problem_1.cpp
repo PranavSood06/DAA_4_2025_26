4dfdd8e6f2d3efd2458986b2cc2249e3676aabf7
PranavSood06 <soodpranav066@gmail.com>
2026-04-27 16:26:08 +0530

    est prac

--- EST Prac/Problem_1.cpp
+++ EST Prac/Problem_1.cpp
@@ -1,31 +1,35 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int tsp(int n, vector<vector<int>>& cost) {
-    int N = 1<<n;
-    vector<vector<int>> dp(N, vector<int>(n, INT_MAX));
-    
-    dp[1][0] = 0; // start from node 0
-    
-    for(int mask = 1; mask < N; mask++) {
-        for(int u = 0; u < n; u++) {
-            if(mask & (1<<u)) {
-                for(int v = 0; v < n; v++) {
-                    if(!(mask & (1<<v))) {
-                        dp[mask | (1<<v)][v] = min(
-                            dp[mask | (1<<v)][v],
-                            dp[mask][u] + cost[u][v]
-                        );
+class Solution {
+  public:
+    int tsp(vector<vector<int>>& cost) {
+        int n = cost.size();
+        
+        if(n == 1) return 0;
+        
+        int N = 1 << n;
+        vector<vector<int>> dp(N, vector<int>(n, 1e9));
+        
+        dp[1][0] = 0;
+        
+        for(int mask = 1; mask < N; mask++){
+            for(int u = 0; u < n; u++){
+                if(mask & (1 << u)){
+                    for(int v = 0; v < n; v++){
+                        if(!(mask & (1 << v))){
+                            dp[mask | (1 << v)][v] = min(
+                                dp[mask | (1 << v)][v],
+                                dp[mask][u] + cost[u][v]
+                            );
+                        }
                     }
                 }
             }
         }
+        
+        int ans = 1e9;
+        for(int u = 1; u < n; u++){
+            ans = min(ans, dp[N-1][u] + cost[u][0]);
+        }
+        
+        return ans;
     }
-    
-    int ans = INT_MAX;
-    for(int u = 1; u < n; u++) {
-        ans = min(ans, dp[N-1][u] + cost[u][0]);
-    }
-    
-    return ans;
-}
+};
--- EST Prac/Problem_2.cpp
+++ EST Prac/Problem_2.cpp
@@ -1,33 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Edge{
-    int u,v,w;
-};
-
-int main(){
-    int V = 5;
-    vector<Edge> edges = {
-        {0,1,4},{0,2,5},{1,2,-3}
-    };
-
-    vector<int> dist(V, INT_MAX);
-    dist[0] = 0;
-
-    for(int i=0;i<V-1;i++){
-        for(auto e: edges){
-            if(dist[e.u]!=INT_MAX && dist[e.u]+e.w<dist[e.v]){
-                dist[e.v] = dist[e.u]+e.w;
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        
+        vector<int> dist(V, 1e8);
+        dist[src] = 0;
+        
+        for(int i = 0; i < V-1; i++){
+            for(auto e : edges){
+                int u = e[0], v = e[1], w = e[2];
+                
+                if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w;
+                }
             }
         }
-    }
-
-    for(auto e: edges){
-        if(dist[e.u]!=INT_MAX && dist[e.u]+e.w<dist[e.v]){
-            cout<<"Negative cycle";
-            return 0;
+        
+        for(auto e : edges){
+            int u = e[0], v = e[1], w = e[2];
+            
+            if(dist[u] != 1e8 && dist[u] + w < dist[v]){
+                return {-1};
+            }
         }
+        
+        return dist;
     }
-
-    for(int i=0;i<V;i++) cout<<dist[i]<<" ";
-}
+};
